1fc6aa58c9c8de79002df594b8dfe1ba90ef86c2
I02eccad <i02eccad@uco.es>
2025-10-13 23:01:51 +0200

    Ajuste de rutas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
 const root = createRoot(document.getElementById('root'));
+
+const RouterComponent = window.location.protocol === 'file:' ? HashRouter : BrowserRouter;
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <RouterComponent>
       <App />
-    </BrowserRouter>
+    </RouterComponent>
   </React.StrictMode>
 );
